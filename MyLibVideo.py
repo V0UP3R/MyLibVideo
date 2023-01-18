6b6653a39081b40ebcd6da44ad6ab23087a06867
V0UP3R <einsteinforcomputer@gmail.com>
2023-01-18 17:57:22 -0300

    MYLIBVIDEO-0000: CONSTRUÇÃO DO RETORNO DO DOWNLOAD

--- MyLibVideo.py
+++ MyLibVideo.py
@@ -3,17 +3,27 @@ from pytube import YouTube
 
 app = Flask(__name__)
 
-@app.route('/home')
+@app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route('/downloadVideo', methods=['POST',])
 def downloadVídeo():
-    url = request.form['url']
-    yt = YouTube(url)
-    for stream in yt.streams:
-        print(stream)
-    video = yt.streams.get_highest_resolution()
-    video.download('C:/Users/einst/Downloads')
-
+    try:
+        url = request.form['url']
+        yt = YouTube(url)
+        # for stream in yt.streams:
+        #     print(stream)
+        video = yt.streams.get_highest_resolution()
+        video.download('C:/Users/einst/Downloads')
+        audio = yt.streams.filter(only_audio=True)[0]
+        title = yt.title + '.mp3'
+        audio.download('C:/Users/einst/Downloads', filename=title)
+        if(video.download('C:/Users/einst/Downloads')):
+            success = 'Sucesso ao baixar vídeo'
+        else:
+            success = 'Falha ao baixar vídeo'
+        return render_template('index.html')
+    except Exception as error:
+        print(error)
 app.run()
